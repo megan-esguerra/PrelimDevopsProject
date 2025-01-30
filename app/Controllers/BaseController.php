5bca603434365be35cfc8374b8d38123ade5e30c
Macabali-Adrian-Mhaki-Enorme <adrianmhakimacabali@gmail.com>
2025-01-30 21:56:44 +0800

    update in order page

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -56,13 +56,3 @@ abstract class BaseController extends Controller
         // E.g.: $this->session = \Config\Services::session();
     }
 }
-protected $orderModel;
-
-public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
-{
-    parent::initController($request, $response, $logger);
-
-    // Load the OrderModel
-    $this->orderModel = new \App\Models\OrderModel();
-}
-
