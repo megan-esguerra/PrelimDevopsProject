ccfc635110d95cbb974418d0e0b40c93317e1156
Macabali-Adrian-Mhaki-Enorme <adrianmhakimacabali@gmail.com>
2025-01-30 22:12:39 +0800

    update in order page

--- app/Views/orders/index.php
+++ app/Views/orders/index.php
@@ -101,7 +101,7 @@
 </head>
 <body>
     <!-- Sidebar -->
-    <?php include('path_to_views/layout/sidebar.php'); ?>
+    <?php include(APPPATH . 'Views/layout/sidebar.php'); ?>
 
     <!-- Main Content -->
     <div class="main-content">
