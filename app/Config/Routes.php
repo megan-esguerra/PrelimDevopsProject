beb7265e682878c2251cf92429fb836d9c40633f
Dark-Goddess <0906megan64@gmail.com>
2025-01-30 14:18:00 +0800

    testing config

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,6 +8,10 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('/register', 'RegisterController::index');
 $routes->post('/register/store', 'RegisterController::store');
-$routes->get('/login', 'LoginController::index'); 
+$routes->get('/login', 'Auth::login'); 
+$routes->post('/login', 'Auth::login');
+$routes->get('/logout', 'Auth::logout');
 $routes->get('dashboard', 'DashboardController::index');
+
+
 
