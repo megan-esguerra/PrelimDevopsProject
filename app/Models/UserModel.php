5d85c34fec9ac7c15f22fd20ee29c8293c592615
KristelAriasCulala <kristelariasculala@gmail.com>
2025-02-02 22:01:01 +0800

    register fuction test 2

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,9 +7,10 @@ class UserModel extends Model {
     protected $table = 'users';
     protected $primaryKey = 'user_id';
 
-    protected $allowedFields = ['first_name', 'last_name', 'email', 'phone', 'password_hash', 'role', 'created_at'];
+    protected $allowedFields = ['first_name', 'last_name', 'email', 'phone', 'password_hash', 'role', 'created_at', 'updated_at'];
     protected $useTimestamps = true;
     protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
 
     protected $beforeInsert = ['hashPassword'];
     protected $beforeUpdate = ['hashPassword'];
