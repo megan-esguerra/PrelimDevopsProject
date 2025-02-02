7ba65f4544e257d233fc64d7b6b2cd7b100fd167
Astr-oIT <estacio.gabrielle31@gmail.com>
2025-02-02 20:21:48 +0800

    sidebar active fix

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -1,10 +1,10 @@
 <div id="sidebar" class="sidebar">
     <ul>
         <li class="Ims"><span class="sideLine">|</span> Cat Cafes Inventory System</li>
-        <li><a href="<?= base_url('dashboard'); ?>" class="<?= service('request')->getUri()->getPath() == '/index.php/dashboard' ? 'active' : '' ?>"><i class='bx bxs-dashboard'></i>Dashboard</a></li>
-        <li><a href="<?= base_url('stock'); ?>" class="<?= service('request')->getUri()->getPath() == '/index.php/stock' ? 'active' : '' ?>"><i class='bx bxs-package'></i>In Stock</a></li>
-        <li><a href="<?= base_url('statistics'); ?>" class="<?= service('request')->getUri()->getPath() == '/index.php/statistics' ? 'active' : '' ?>"><i class='bx bx-bar-chart-square' style='color:#ffffff' ></i>Statistics</a></li>
-        <li><a href="<?= base_url('orders'); ?>" class="<?= service('request')->getUri()->getPath() == '/index.php/orders' ? 'active' : '' ?>"><i class='bx bxs-cart-download'></i>Orders</a></li>
-        <li><a href="<?= base_url('users'); ?>" class="<?= service('request')->getUri()->getPath() == '/index.php/users' ? 'active' : '' ?>"><i class='bx bxs-user-account'></i>Users</a></li>
+        <li><a href="<?= base_url('dashboard'); ?>" class="<?= service('request')->getUri()->getPath() == '/dashboard' ? 'active' : '' ?>"><i class='bx bxs-dashboard'></i>Dashboard</a></li>
+        <li><a href="<?= base_url('stock'); ?>" class="<?= service('request')->getUri()->getPath() == '/stock' ? 'active' : '' ?>"><i class='bx bxs-package'></i>In Stock</a></li>
+        <li><a href="<?= base_url('statistics'); ?>" class="<?= service('request')->getUri()->getPath() == '/statistics' ? 'active' : '' ?>"><i class='bx bx-bar-chart-square' style='color:#ffffff' ></i>Statistics</a></li>
+        <li><a href="<?= base_url('orders'); ?>" class="<?= service('request')->getUri()->getPath() == '/orders' ? 'active' : '' ?>"><i class='bx bxs-cart-download'></i>Orders</a></li>
+        <li><a href="<?= base_url('users'); ?>" class="<?= service('request')->getUri()->getPath() == '/users' ? 'active' : '' ?>"><i class='bx bxs-user-account'></i>Users</a></li>
     </ul>
 </div>
