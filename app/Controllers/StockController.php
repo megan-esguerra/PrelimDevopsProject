05794d1c35a0dc983d7b91261c7b04eb2ab8e42a
Astr-oIT <estacio.gabrielle31@gmail.com>
2025-02-05 06:46:57 +0800

    fixing stock

--- app/Controllers/StockController.php
+++ app/Controllers/StockController.php
@@ -9,7 +9,7 @@ class StockController extends BaseController
 {
     public function index()
     {
-        $stockModel = new StockModel();
+        $stockModel = new ProductModel();
         $data['products'] = $stockModel->getInStockItems(); // Fetch in-stock items
 
         return view('instock', $data);
