60f6164bb67455fdc008c4830545cf8f0e864755
Macabali-Adrian-Mhaki-Enorme <adrianmhakimacabali@gmail.com>
2025-02-04 16:26:43 +0800

    ..

--- app/Controllers/Orders.php
+++ app/Controllers/Orders.php
@@ -4,20 +4,17 @@ namespace App\Controllers;
 use App\Models\OrderModel;
 use App\Models\CustomerModel;
 use App\Models\SupplierModel;
+use CodeIgniter\Controller;
 
 class Orders extends BaseController {
     public function index() {
         $orderModel = new OrderModel();
         $orders = $orderModel
             ->select('orders.*, customers.customer_name, suppliers.supplier_name')
-            ->join('customers', 'customers.id = orders.customer_id')
-            ->join('suppliers', 'suppliers.id = orders.supplier_id')
+            ->join('customers', 'customers.customer_id = orders.customer_id')
+            ->join('suppliers', 'suppliers.supplier_id = orders.supplier_id')
             ->findAll();
 
-        return view('orders', ['orders' => $orders]);
-    }
-
-    public function newOrder() {
-        return view('new_order');
+        return view('Pages/orders', ['orders' => $orders]);
     }
 }
