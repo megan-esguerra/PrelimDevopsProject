74d149a7b0ecb650fa0092e1a3cee3bcfd6afd45
Astr-oIT <estacio.gabrielle31@gmail.com>
2025-02-05 11:36:58 +0800

    fixing stock

--- app/Controllers/StockController.php
+++ app/Controllers/StockController.php
@@ -2,36 +2,16 @@
 
 namespace App\Controllers;
 
-use App\Models\StockModel;
-use CodeIgniter\Controller;
+use App\Models\ProductModel;
 
-class StockController extends Controller
+
+class StockController extends BaseController
 {
     public function index()
     {
-        $stockModel = new StockModel();
-        $data['products'] = $stockModel->findAll();
+        $stockModel = new ProductModel();
+        $data['products'] = $stockModel->getInStockItems(); // Fetch in-stock items
+
         return view('Pages/instock', $data);
-    }
-
-    public function store()
-    {
-        $stockModel = new StockModel();
-
-        $data = [
-            'order_id' => $this->request->getPost('order_id'),
-            'product' => $this->request->getPost('product'),
-            'category' => $this->request->getPost('category'),
-            'sales_channel' => $this->request->getPost('sales_channel'),
-            'instruction' => $this->request->getPost('instruction'),
-            'items' => $this->request->getPost('items'),
-            'status' => $this->request->getPost('status'),
-        ];
-
-        if ($stockModel->insert($data)) {
-            return $this->response->setJSON(['success' => true]);
-        } else {
-            return $this->response->setJSON(['success' => false]);
-        }
     }
 }
